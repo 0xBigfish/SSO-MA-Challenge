f883224addfd8b30a7eefa916f938f647e01af9c
0xBigfish <46397578+0xBigfish@users.noreply.github.com>
2025-08-09 00:58:57 +0200

    add button to redirect to log page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import router from "./router/index.js";
+function showLogs() {
+  router.push({path:'/logs'})
+}
 </script>
 
 <template>
+  <div><button @click="showLogs">Show Logs</button></div>
+  <div><strong>Warning: Starting any flow will clear the logs</strong></div>
   <router-view/>
 </template>
 
