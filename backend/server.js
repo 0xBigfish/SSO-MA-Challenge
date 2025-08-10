4667fd0bfa53bc008f3ad0084869937d4ee8f1d4
0xBigfish <46397578+0xBigfish@users.noreply.github.com>
2025-08-11 00:39:43 +0200

    make vue available on 127.0.0.1 and localhost

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ require('./utils/axiosLogger');
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials: true,
 }));
 app.use(express.json());
